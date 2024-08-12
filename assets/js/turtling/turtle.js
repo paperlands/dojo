fd0ea77a0d77ee36b04f24e891727e3a6d3c7066
ks0m1c_dharma <sakiyamuni@sams.ara>
2024-08-13 04:09:12 +0800

    quaternions

--- assets/js/turtling/turtle.js
+++ assets/js/turtling/turtle.js
@@ -1,5 +1,6 @@
 import { Parser } from "./mafs/parse.js"
 import { Evaluator } from "./mafs/evaluate.js"
+import { Versor } from "./mafs/versors.js"
 
 export class Turtle {
     constructor(canvas) {
@@ -10,6 +11,9 @@ export class Turtle {
             fw: this.forward.bind(this),
             rt: this.right.bind(this),
             lt: this.left.bind(this),
+            yaw: this.yaw.bind(this),
+            pitch: this.pitch.bind(this),
+            roll: this.roll.bind(this),
             show: this.unhideTurtle.bind(this),
             hd: this.hideTurtle.bind(this),
             jmp: this.jmp.bind(this),
@@ -17,6 +21,13 @@ export class Turtle {
         };
         this.functions = {};
 
+        this.camera = {
+            x: 0,
+            y: 0,
+            z: 500
+        };
+
+        this.rotation = new Versor(1, 0, 0, 0); // Identity quaternion
         // Command execution tracking
         this.commandCount = 0;
         this.maxCommands = 5000;
@@ -31,8 +42,9 @@ export class Turtle {
     }
 
     spawn() {
-        this.x = this.ctx.canvas.width / 2;
-        this.y = this.ctx.canvas.height / 2;
+        this.x = 0;
+        this.y = 0;
+        this.z = 0
     }
 
     defineFunction(name, parameters, body) {
@@ -110,6 +122,7 @@ export class Turtle {
         const scrolly = (this.y > window.innerHeight) && (this.y + window.innerHeight / 2) || this.y
         window.scrollTo(this.x , scrolly);
         this.angle = 0;
+        this.rotation = new Versor(1, 0, 0, 0);
         this.penDown = true;
         this.color = 'red';
         this.ctx.strokeStyle = this.color;
@@ -119,24 +132,56 @@ export class Turtle {
     }
 
     forward(distance) {
-        const newX = this.x + distance * Math.cos(this.angle * Math.PI / 180);
-        const newY = this.y + distance * Math.sin(this.angle * Math.PI / 180);
+        const direction = { x: 1, y: 0, z: 0 }; // Forward direction in local space
+
+        const rotatedDirection = this.rotation.rotate(direction);
+        const newX = this.x + rotatedDirection.x * distance;
+        const newY = this.y + rotatedDirection.y * distance;
+        const newZ = this.z + rotatedDirection.z * distance;
+
         if (this.penDown) {
             this.ctx.beginPath();
-            this.ctx.moveTo(this.x, this.y);
-            this.ctx.lineTo(newX, newY);
+            this.ctx.moveTo(this.projectX(this.x, this.z), this.projectY(this.y, this.z));
+            this.ctx.lineTo(this.projectX(newX, newZ), this.projectY(newY, newZ));
             this.ctx.stroke();
         }
+
         this.x = newX;
         this.y = newY;
+        this.z = newZ;
     }
 
-    right(angle) {
-        this.angle += angle;
+    projectX(x, z) {
+        const perspective = this.camera.z / (this.camera.z - z);
+        return (x - this.camera.x) * perspective + this.ctx.canvas.width / 2;
+    }
+
+    projectY(y, z) {
+        const perspective = this.camera.z / (this.camera.z - z);
+        return (y - this.camera.y) * perspective + this.ctx.canvas.height / 2;
+    }
+
+    roll(angle) {
+        const rotation = Versor.fromAxisAngle({ x: 1, y: 0, z: 0 }, angle);
+        this.rotation = rotation.multiply(this.rotation);
+    }
+
+    pitch(angle) {
+        const rotation = Versor.fromAxisAngle({ x: 0, y: 1, z: 0 }, angle);
+        this.rotation = rotation.multiply(this.rotation);
+    }
+
+    yaw(angle) {
+        const rotation = Versor.fromAxisAngle({ x: 0, y: 0, z: 1 }, angle);
+        this.rotation = rotation.multiply(this.rotation);
     }
 
     left(angle) {
-        this.angle -= angle;
+        this.yaw(-angle);
+    }
+
+    right(angle) {
+        this.yaw(angle);
     }
 
     jmp(distance){
@@ -157,23 +202,57 @@ export class Turtle {
     drawTurtle() {
         if (this.showTurtle) {
             const headSize = 10;
+            const projectedX = this.projectX(this.x, this.z);
+            const projectedY = this.projectY(this.y, this.z);
 
             this.ctx.save();
             this.ctx.fillStyle = this.color;
-            this.ctx.translate(this.x, this.y);
-            this.ctx.rotate(this.angle * Math.PI / 180);
+            this.ctx.translate(projectedX, projectedY);
+
+            // Convert quaternion to rotation matrix
+            const transformValues = this.rotation.getTransformValues();
+            this.ctx.transform(transformValues.a, transformValues.b, transformValues.c, transformValues.d, transformValues.e, transformValues.f);
 
             this.ctx.beginPath();
-            this.ctx.moveTo(headSize, 0);
-            this.ctx.lineTo(-headSize / 2, headSize / 2);
-            this.ctx.lineTo(-headSize / 2, -headSize / 2);
+
+            // Draw turtle-like shape (triangle)
+            this.ctx.moveTo(headSize, 0); // Pointing direction
+            this.ctx.lineTo(-headSize / 2, headSize / 2); // Bottom left
+            this.ctx.lineTo(-headSize / 2, -headSize / 2); // Bottom right
+
             this.ctx.closePath();
+            this.ctx.fill(); // Fill the turtle
+                             //
+                             //
+            // Stroke the outline of the turtle shape
+            this.ctx.strokeStyle = 'black'; // Outline color
+            this.ctx.stroke();
+
+            //front eyes
+            this.ctx.beginPath();
+            this.ctx.arc(1, headSize / 2 , 2, 0, Math.PI * 2);
+            this.ctx.arc(5, -headSize / 2 , 2, 0, Math.PI * 2);
+            this.ctx.fillStyle = this.color; // Color of the top indicator
             this.ctx.fill();
 
+            // // Optionally, draw a small circle to indicate the top of the turtle inverse of a rotation matrix is the rotation matrix's transpose.
+            console.log(transformValues)
+            this.ctx.resetTransform(1,0,0,1,0,0)
+            this.ctx.translate(projectedX, projectedY);
+            // this.ctx.scale(1 / transformValues.a, 1 / transformValues.d);
+            // const scaleFactor = 1 / Math.max(transformValues.a, transformValues.d);
+            // this.ctx.scale(scaleFactor, scaleFactor);
+            this.ctx.beginPath();
+            this.ctx.arc(1, headSize / 2 , 1, 0, Math.PI);
+            this.ctx.arc(5, -headSize / 2 , 1, 0, Math.PI);
+            this.ctx.arc(0, 0, 3, 0, Math.PI * 2, true);
+            // Circle above the turtle
+            this.ctx.closePath();
+            this.ctx.fillStyle = this.color; // Color of the top indicator
+            this.ctx.fill();
             this.ctx.restore();
-
-        }}
-
+        }
+    }
     hideTurtle() {
         this.showTurtle = false;
     }
