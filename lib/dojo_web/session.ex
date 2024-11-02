9135722bee09e793d7ba26acf4b1fc2f9d307e1b
ks0m1c_dharma <sakiyamuni@sams.ara>
2024-11-02 15:15:30 +0800

    sessioning

--- lib/dojo_web/session.ex
+++ lib/dojo_web/session.ex
@@ -3,13 +3,13 @@ defmodule DojoWeb.Session do
   import Phoenix.LiveView, only: [get_connect_params: 1]
 
   @derive Jason.Encoder
-  defstruct name: nil, id: nil, active: false
+  defstruct name: nil, id: nil, active: false, class: nil, last_opened: DateTime.now!("Etc/UTC")
 
   @default_locale "en"
   @timezone "UTC"
   @timezone_offset 0
 
-  def on_mount(:anon, _params, _sessions, socket) do
+  def on_mount(:anon, params, _sessions, socket) do
     # get_connect_params returns nil on the first (static rendering) mount call, and has the added connect params from the js LiveSocket creation on the subsequent (LiveSocket) call
     #
     {:cont,
@@ -18,22 +18,43 @@ defmodule DojoWeb.Session do
      locale: get_connect_params(socket)["locale"] || @default_locale,
      tz: %{timezone: get_connect_params(socket)["timezone"] || @timezone,
            timezone_offset: get_connect_params(socket)["timezone_offset"] || @timezone_offset},
-     session: get_connect_params(socket)["session"] |> mutate_session()
+     session: get_connect_params(socket)["session"] |> mutate_session(params)
      )}
   end
 
 
+    defp mutate_session(%{"id" => id} = sess, %{"clan" => clan})
+       when is_binary(id) and is_binary(clan) do
+    atomised_sess =
+      for {key, val} <- sess, reduce: %{} do
+        acc -> hydrate_session(acc, key, val)
+      end
 
-  # careful of client and server state
-  defp mutate_session(%{"id" => id} = sess) when is_binary(id) do
-    atomised_sess = for {key, val} <- sess, into: %{} do
-      {String.to_existing_atom(key), val}
-    end
-    %{struct(%__MODULE__{}, atomised_sess)| active: true}
+    struct(%__MODULE__{}, atomised_sess)
   end
-  #explicit active from clientside
-  defp mutate_session(%{"active" => true} = sess), do: %__MODULE__{id: :crypto.strong_rand_bytes(18) |> :base64.encode(), active: true}
-  defp mutate_session(sess) when is_map(sess), do: %__MODULE__{id: :crypto.strong_rand_bytes(18) |> :base64.encode(), active: true}
+
+  # careful of client and server state race. id here is not SOT
+  defp mutate_session(%{"active" => true} = sess, _) do
+    atomised_sess =
+      for {key, val} <- sess, reduce: %{} do
+        acc -> hydrate_session(acc, key, val)
+      end
+
+    struct(%__MODULE__{}, atomised_sess)
+  end
+
   # false first load
-  defp mutate_session(sess), do: %__MODULE__{}
-end
+  defp mutate_session(_, _), do: %__MODULE__{}
+
+  defp hydrate_session(acc, "clan", %{"id" => id}) do
+    put_in(acc, [:clan], Dojo.Clan.get(id))
+  end
+
+  defp hydrate_session(acc, key, val) when key in ["name", "id", "active", "last_opened"] do
+    put_in(acc, [String.to_existing_atom(key)], val)
+  end
+
+  defp hydrate_session(acc, _key, _val) do
+     acc
+  end
+ end
