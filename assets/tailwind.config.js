5abc607d9333ba8a214615b87db838d49b27fe1f
ks0m1c_dharma <sakiyamuni@sams.ara>
2024-12-19 16:07:50 +0800

    fix scrollbars

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -22,7 +22,6 @@ module.exports = {
     },
   },
   plugins: [
-    require("tailwind-scrollbar-hide"),
     require("@tailwindcss/forms"),
     // Allows prefixing tailwind classes with LiveView classes to add rules
     // only when LiveView classes are applied, for example:
