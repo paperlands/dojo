da7f176dceb0d29fa97da8dbd34838baa2e68f9d
ks0m1c_dharma <sakiyamuni@sams.ara>
2025-01-04 20:56:37 +0800

    paperland sg

--- config/config.exs
+++ config/config.exs
@@ -20,7 +20,7 @@ config :dojo, DojoWeb.Endpoint,
     layout: false
   ],
   pubsub_server: Dojo.PubSub,
-  check_origin: ["https://dojo.paperland.in", "https://thedojo.fly.dev"],
+  check_origin: ["https://dojo.paperland.sg", "https://dojo.paperland.in", "https://thedojo.fly.dev"],
   live_view: [signing_salt: "ko/5Xrfn"]
 
 # Configures the mailer
