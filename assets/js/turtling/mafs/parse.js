c94ca9d85790c8b3939e978a76022b6fbdd290b6
ks0m1c_dharma <sakiyamuni@sams.ara>
2025-01-08 21:41:08 +0800

    support mafs fns

--- assets/js/turtling/mafs/parse.js
+++ assets/js/turtling/mafs/parse.js
@@ -7,33 +7,57 @@ export class Parser {
             '+': 1, '-': 1,
             '*': 2, '/': 2,
             '^': 3,
-            '>': 4,'=': 4,'<': 4,
+            '>': 4, '=': 4, '<': 4,
             '|': 5,
             '&': 6
         };
+
+        // Add built-in functions
+        this.functions = new Set([
+            'sin', 'cos', 'tan',
+            'asin', 'acos', 'atan',
+            'sqrt', 'log', 'exp'
+        ]);
     }
 
     run(expression) {
         const tokens = this.tokenise(expression);
         const output = [];
         const operators = [];
+        let expectFunction = false;
 
-        for (const token of tokens) {
-            if (this.isOperand(token)) {
+        for (let i = 0; i < tokens.length; i++) {
+            const token = tokens[i];
+
+            if (this.isFunction(token)) {
+                operators.push(token);
+                expectFunction = true;
+            } else if (this.isOperand(token)) {
                 output.push(new ASTNode('operand', token));
             } else if (token in this.precedence) {
                 while (operators.length > 0 &&
-                       this.precedence[operators[operators.length - 1]] >= this.precedence[token]) {
+                    this.precedence[operators[operators.length - 1]] >= this.precedence[token]) {
                     this.createOperatorNode(operators, output);
                 }
                 operators.push(token);
             } else if (token === '[') {
+                if (expectFunction) {
+                    expectFunction = false;
+                }
                 operators.push(token);
             } else if (token === ']') {
                 while (operators.length > 0 && operators[operators.length - 1] !== '[') {
                     this.createOperatorNode(operators, output);
                 }
-                operators.pop(); // Remove '('
+                if (operators.length > 0) {
+                    operators.pop(); // Remove '['
+                    if (operators.length > 0 && this.isFunction(operators[operators.length - 1])) {
+                        // Create function node
+                        const func = operators.pop();
+                        const arg = output.pop();
+                        output.push(new ASTNode('function', func, [arg]));
+                    }
+                }
             }
         }
 
@@ -41,10 +65,11 @@ export class Parser {
             this.createOperatorNode(operators, output);
         }
 
-        return output[0]; // The root of the AST
+        return output[0];
     }
 
     tokenise(expression) {
+        // Enhanced tokenizer to handle function names
         return expression.match(/\d+\.?\d*|[a-zA-Z]+|\S/g) || [];
     }
 
@@ -58,10 +83,19 @@ export class Parser {
             !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
     }
 
+    isFunction(token) {
+        return this.functions.has(token);
+    }
+
     createOperatorNode(operators, output) {
         const operator = operators.pop();
-        const right = output.pop();
-        const left = output.pop();
-        output.push(new ASTNode('operator', operator, [left, right]));
+        if (this.isFunction(operator)) {
+            const arg = output.pop();
+            output.push(new ASTNode('function', operator, [arg]));
+        } else {
+            const right = output.pop();
+            const left = output.pop();
+            output.push(new ASTNode('operator', operator, [left, right]));
+        }
     }
 }
