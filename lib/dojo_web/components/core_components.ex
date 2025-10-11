df9725a54a581d10c1311fc6db05112662f607ae
ks0m1c_dharma <sakiyamuni@sams.ara>
2025-10-11 19:41:02 +0800

    outershell css fixes

--- lib/dojo_web/components/core_components.ex
+++ lib/dojo_web/components/core_components.ex
@@ -580,25 +580,18 @@ defmodule DojoWeb.CoreComponents do
   def theme_toggle(assigns) do
     ~H"""
     <div class="relative flex flex-row items-center border-2 border-base-300 rounded-full">
-      <div class="absolute w-1/3 h-full rounded-full bg-accent/50 left-0 [[data-theme=light]_&]:left-1/3 [[data-theme=dark]_&]:left-2/3 transition-[left]" />
-
-      <button
-        phx-click={JS.dispatch("phx:set-theme", detail: %{theme: "system"})}
-        class="flex p-2 cursor-pointer w-1/3"
-      >
-        <.icon name="hero-computer-desktop-micro" class="size-4 opacity-75 hover:opacity-100" />
-      </button>
+      <div class="absolute w-1/2 h-full rounded-full bg-accent/50 left-0 [[data-theme=light]_&]:left-0 [[data-theme=dark]_&]:left-1/2 transition-[left]" />
 
       <button
         phx-click={JS.dispatch("phx:set-theme", detail: %{theme: "light"})}
-        class="flex p-2 cursor-pointer w-1/3"
+        class="flex p-2 cursor-pointer w-1/2"
       >
         <.icon name="hero-sun-micro" class="size-4 opacity-75 hover:opacity-100" />
       </button>
 
       <button
         phx-click={JS.dispatch("phx:set-theme", detail: %{theme: "dark"})}
-        class="flex p-2 cursor-pointer w-1/3"
+        class="flex p-2 cursor-pointer w-1/2"
       >
         <.icon name="hero-moon-micro" class="size-4 opacity-75 hover:opacity-100" />
       </button>
