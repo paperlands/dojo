92303f827eee01ec96792ed9a72cbe87a1c917b0
ks0m1c_dharma <sakiyamuni@sams.ara>
2026-04-08 03:22:05 +0800

    amber twilight

--- lib/dojo_web/live/shell_live.ex
+++ lib/dojo_web/live/shell_live.ex
@@ -628,7 +628,7 @@ defmodule DojoWeb.ShellLive do
     <!-- Command Deck Component (command_deck.html.heex) -->
     <div
       id="commanddeck"
-      class="rightthird fixed right-0 flex deck mt-[15dvh] h-3/5 lg:h-4/5 select-none animate-fade hidden sm:block"
+      class="rightthird fixed right-0 flex deck mt-[15dvh] h-3/5 lg:h-4/5 select-none animate-fade sm:block"
       phx-update="ignore"
     >
       <!-- Command Deck Panel -->
