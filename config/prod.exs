e34f1c272ff2ac392402cd451ce58fe13021ace5
ks0m1c_dharma <johndoe@dharma.in>
2026-04-19 16:03:48 +0800

    Prod DNS Config

--- config/prod.exs
+++ config/prod.exs
@@ -16,5 +16,9 @@ config :swoosh, local: false
 # Do not print debug messages in production
 config :logger, level: :info
 
+# Prod uses DNS-based peer discovery — no mDNS engine, no local network services
+config :dojo, :discovery, :dns
+config :dojo, :cluster_adapter, Dojo.Cluster.DNS.Adapter
+
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
